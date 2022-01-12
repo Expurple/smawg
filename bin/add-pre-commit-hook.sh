582c76f3672138479fac500f9770d00e53af2c3b
Dmitry Alexandrov <adk230@yandex.ru>
2022-01-12 22:33:53 +0300

    enforce flake8-docstrings

--- bin/add-pre-commit-hook.sh
+++ bin/add-pre-commit-hook.sh
@@ -7,6 +7,11 @@ echo 'Running mypy:'
 mypy --config-file= smawg/ || exit 1
 echo
 
+# https://stackoverflow.com/questions/70680757
+echo 'Checking if flake8-docstrings is installed:'
+flake8 --version | grep -q 'flake8-docstrings' || exit 1
+echo
+
 echo 'Running flake8:'
 flake8 --config=setup.cfg smawg/ || exit 1
 echo
